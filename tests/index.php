1a1379c1e3778ae69d832b4037cce18745629fc3
fmihel <fmihel76@gmail.com>
2024-01-29 08:25:25 +0300

    remove config.php

--- tests/index.php
+++ tests/index.php
@@ -1,17 +1,15 @@
-<?php 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/funcs.php';
-require_once __DIR__.'/../source/Cache.php';
-require_once __DIR__.'/../source/Stat.php';
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/funcs.php';
+require_once __DIR__ . '/../source/Cache.php';
+require_once __DIR__ . '/../source/Stat.php';
 
-use fmihel\console;
-use fmihel\config\Config;
 use fmihel\base\Base;
 use fmihel\cache\Cache;
-use fmihel\cache\Stat;
 use fmihel\cache\drivers\FileCacheDriver;
 use fmihel\cache\drivers\SerCacheDriver;
-
+use fmihel\config\Config;
+use fmihel\console;
 
 console::line();
 
@@ -21,13 +19,12 @@ foreach (Config::get('base') as $base) {
 
 console::line();
 console::log('FileCacheDriver');
-Cache::setDriver(new FileCacheDriver(__DIR__.'/cache'));
-include __DIR__.'/file.php';
+Cache::setDriver(new FileCacheDriver(__DIR__ . '/cache'));
+include __DIR__ . '/file.php';
 
 console::line();
 console::log('SerCacheDriver');
-Cache::setDriver(new SerCacheDriver(__DIR__.'/cache'));
-include __DIR__.'/file.php';
+Cache::setDriver(new SerCacheDriver(__DIR__ . '/cache'));
+include __DIR__ . '/file.php';
 
 console::line();
-
